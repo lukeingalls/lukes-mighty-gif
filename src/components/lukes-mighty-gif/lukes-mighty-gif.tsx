c909748e82d1808495d5b0f939eef2427db78086
Luke Ingalls <lukeingalls@me.com>
2022-12-19 20:57:16 -0700

    remove comments

--- src/components/lukes-mighty-gif/lukes-mighty-gif.tsx
+++ src/components/lukes-mighty-gif/lukes-mighty-gif.tsx
@@ -59,62 +59,6 @@ export class LukesMightyGif {
     if (this.src) this.setIsGif(this.src);
   }
 
-  // async downloadGif() {
-  //   try {
-  //     await this.isGif();
-  //     const getGif = new XMLHttpRequest();
-  //     getGif.responseType = 'arraybuffer';
-  //     getGif.onload = request => {
-  //       const downloadReady = handleGIF(request.target.response);
-  //     };
-  //     getGif.onprogress = e => e.lengthComputable && downloadBar.set(e.loaded / e.total);
-  //     getGif.onerror = showError.bind(null, validURL);
-  //     getGif.open('GET', this.src, true);
-  //     getGif.send();
-  //     url = validURL;
-  //   } catch (e) {
-  //     this.onError.emit(e);
-  //   }
-  // }
-
-  // handleGif() {
-  //   console.timeEnd('download');
-  //   console.time('parse');
-  //   const bytes = new Uint8Array(buffer);
-  //   init();
-
-  //   // Image dimensions
-  //   const dimensions = new Uint16Array(buffer, 6, 2);
-  //   [state.width, state.height] = dimensions;
-  //   canvas.render.width = canvas.display.width = state.width;
-  //   canvas.render.height = canvas.display.height = state.height;
-  //   dom.bar[0].style.width = dom.line[0].style.width = state.barWidth = Math.max(state.width, 450);
-  //   $('#content').css({ width: state.barWidth, height: state.height });
-
-  //   // Adjust window size
-  //   if (!preference('open-tabs')) {
-  //     chrome.windows.getCurrent(win => {
-  //       chrome.windows.update(win.id, {
-  //         width: Math.max(state.width + 180, 640),
-  //         height: clamp(state.height + 300, 410, 850),
-  //       });
-  //     });
-  //   }
-
-  //   // Record global color table
-  //   let pos = 13 + colorTableSize(bytes[10]);
-  //   const gct = bytes.subarray(13, pos);
-
-  //   state.frames = parseFrames(buffer, pos, gct, state.keyFrameRate);
-  //   console.timeEnd('parse');
-
-  //   return renderKeyFrames()
-  //     .then(showControls)
-  //     .then(renderIntermediateFrames)
-  //     .then(explodeFrames)
-  //     .catch(err => console.error('Rendering GIF failed!', err));
-  // }
-
   render() {
     return (
       <Host>
