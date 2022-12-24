b6c56cc20d775a1899a5e1697cffefc1936062aa
Luke Ingalls <lukeingalls@me.com>
2022-12-23 18:36:42 -0700

    hide control panel

--- src/components/lukes-mighty-gif/lukes-mighty-gif.tsx
+++ src/components/lukes-mighty-gif/lukes-mighty-gif.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, Watch, Event, EventEmitter } from '@stencil/core';
+import { Component, Host, h, Prop, Watch, Event, EventEmitter, State } from '@stencil/core';
 import Gif from '../helpers';
 
 @Component({
@@ -9,14 +9,15 @@ import Gif from '../helpers';
 })
 export class LukesMightyGif {
   gif: Gif;
+  startedPlayingAt = 0;
+  resetShowControlsIntervalRef: number | null;
+  ref: HTMLDivElement | null = null;
 
   @Prop() src: string;
   @Prop() controls: boolean;
 
   @Prop({ mutable: true }) currentTime: number;
   @Prop({ mutable: true }) paused: boolean;
-
-  startedPlayingAt = 0;
 
   /**
    * The next few props are meant to be treated as read-only. I don't know
@@ -35,10 +36,33 @@ export class LukesMightyGif {
   @Event({ eventName: 'onload' }) load: EventEmitter;
   @Event({ eventName: 'onprogress' }) progress: EventEmitter;
 
+  @State()
+  shouldShowControls = false;
+
   componentDidLoad() {
     if (this.src) {
       this.handleSrcChange();
     }
+  }
+
+  @Watch('controls')
+  controlsChanged(_newControls: any, _oldControls: any) {
+    const newControls = !!_newControls;
+    const oldControls = !!_oldControls;
+
+    if (newControls === oldControls) return;
+    if (newControls) {
+      this.shouldShowControls = true;
+      return;
+    }
+    this.shouldShowControls = false;
+  }
+
+  @Watch('shouldShowControls')
+  shouldShowControlsChanged(newShouldShowControls: boolean, oldShouldShowControls: boolean) {
+    if (newShouldShowControls === oldShouldShowControls) return;
+    if (!newShouldShowControls) return;
+    this.resetShowControlsInterval();
   }
 
   @Watch('src')
@@ -48,6 +72,7 @@ export class LukesMightyGif {
       this.handleSrcChange();
     }
   }
+
   handleSrcChange() {
     this.gif = new Gif(this.src);
     this.gif.onLoad = () => this.load.emit();
@@ -77,6 +102,15 @@ export class LukesMightyGif {
       this.canplaythrough.emit();
       this.startPlaying();
     };
+  }
+
+  resetShowControlsInterval() {
+    if (this.resetShowControlsIntervalRef) {
+      window.clearTimeout(this.resetShowControlsIntervalRef);
+    }
+    this.resetShowControlsIntervalRef = window.setTimeout(() => {
+      this.shouldShowControls = false;
+    }, 2000);
   }
 
   startPlaying() {
@@ -120,26 +154,39 @@ export class LukesMightyGif {
 
             height: `${this.height}px`,
           }}
+          onMouseEnter={() => {
+            this.shouldShowControls = true;
+            this.resetShowControlsInterval();
+          }}
+          onMouseMove={() => {
+            this.shouldShowControls = true;
+            this.resetShowControlsInterval();
+          }}
+          ref={el => {
+            this.ref = el;
+          }}
         >
           <canvas id="canvas-display" class="gif-canvas" width={this.width} height={this.height}></canvas>
 
-          <div id="control-bar" class="control-bar">
-            <div
-              class="play-pause-button"
-              onClick={() => {
-                if (this.paused) {
-                  this.startPlaying();
-                } else {
-                  this.pause();
-                }
-              }}
-            >
-              {this.paused ? <div class="play-button" /> : <div class="pause-button" />}
+          {this.shouldShowControls && (
+            <div id="control-bar" class="control-bar">
+              <div
+                class="play-pause-button"
+                onClick={() => {
+                  if (this.paused) {
+                    this.startPlaying();
+                  } else {
+                    this.pause();
+                  }
+                }}
+              >
+                {this.paused ? <div class="play-button" /> : <div class="pause-button" />}
+              </div>
+              <div class="progress-bar">
+                <div class="progress-bar__filler" style={{ width: `${(100 * this.currentTime) / this.duration}%` }} />
+              </div>
             </div>
-            <div class="progress-bar">
-              <div class="progress-bar__filler" style={{ width: `${(100 * this.currentTime) / this.duration}%` }} />
-            </div>
-          </div>
+          )}
         </div>
         <div id="render-canvas-mount-point" class="sr-only" />
         <div id="error-message" style={{ color: 'red' }} />
